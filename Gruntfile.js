39eb7806ebe0aa1c3ac6207b386624efb6711984
David Krutsko <dave@krutsko.net>
2015-09-13 06:37:15 -0400

    Added Documentation Parser
    > Separate parameter parsing

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,278 @@ mMarked.setOptions
 
 
 //----------------------------------------------------------------------------//
+// Functions                                                                  //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+
+function parseSrc (content)
+{
+	var parsed = [ ]; // Fully tokenized input
+	var result = [ ]; // The resulting context
+	var duplex = { }; // Operator overloading
+
+	// Standardize and remove all C++ comments
+	content = content.replace (/\r?\n/gm, "\n")
+		  .replace (/(?:^|[^\\])\/\/.*$/gm, "")
+		  .replace (  /\/\*[\s\S]*?\*\//gm, "")
+		  .split (/\n/);
+
+	// Loop through every line in the source
+	for (var l = 0; l < content.length; ++l)
+	{
+		var tokens = content[l]
+			// Split line into tokens
+			.split (/(\s+|\(|\)|;|,)/);
+
+		var first = true;
+		// Loop through every token in the line
+		for (var t = 0; t < tokens.length; ++t)
+		{
+			tokens[t] = tokens[t].trim();
+			// Ignore any empty tokens
+			if (tokens[t].length !== 0)
+			{
+				// Preserve parameter formatting hints
+				if (first === true && parsed.length &&
+					parsed[parsed.length-1] !== ";" &&
+					parsed[parsed.length-1] !== "public:")
+					tokens[t] = "\n " + tokens[t];
+
+				parsed.push (tokens[t]); first = false;
+			}
+		}
+	}
+
+	// Loop through all parsed tokens
+	for (var p = 0; p < parsed.length; )
+	{
+		var token = parsed[p++];
+		if (token === "public:")
+		{
+			// Push separator value
+			if (result.length !== 0)
+				result.push ({ });
+
+			continue;
+		}
+
+		var line = { };
+		// Check static keyword
+		if (token === "static")
+		{
+			line.static = true;
+			token = parsed[p++];
+		}
+
+		// Check virtual keyword
+		if (token === "virtual")
+		{
+			line.virtual = true;
+			token = parsed[p++];
+		}
+
+		// Check explicit keyword
+		if (token === "explicit")
+			token = parsed[p++];
+
+		line.return = "";
+		// Check const keyword
+		if (token === "const")
+		{
+			line.return = "const ";
+			token = parsed[p++];
+		}
+
+		line.return += token;
+		token = parsed[p++];
+
+		if (token === "(")
+		{
+			// Value must be a ctor
+			line.name = line.return;
+			line.return = "";
+		}
+
+		else
+		{
+			// Normal function
+			line.name = token;
+			token = parsed[p++];
+
+			if (token === ";")
+			{
+				// Must be variable
+				result.push (line);
+				continue;
+			}
+
+			// Check for operator function
+			if (line.name === "operator")
+			{
+				if (token === "(")
+				{
+					// Handle operator()
+					line.name += " ()";
+					token = parsed[p++];
+					token = parsed[p++];
+				}
+
+				else
+				{
+					line.name += " " + token;
+					token = parsed[p++];
+				}
+			}
+		}
+
+		if (!duplex[line.name])
+			 duplex[line.name] = 0;
+		   ++duplex[line.name];
+
+		if (token !== "(")
+			// Expecting start of parameters list
+			throw new Error ("Expecting \"(\"");
+		token = parsed[p++];
+
+		line.args = [ ];
+		// Iterate parameters
+		while (token !== ")")
+		{
+			// Empty params list
+			if (token === "void")
+			{
+				token = parsed[p++];
+				continue;
+			}
+
+			var arg = { type: "" };
+			// Check const keyword
+			if (token ===    "const" ||
+				token === "\n const")
+			{
+				arg.type = token + " ";
+				token = parsed[p++];
+			}
+
+			arg.type += token;
+			token = parsed[p++];
+
+			arg.name = token;
+			token = parsed[p++];
+
+			// Assemble default
+			if (token === "=")
+			{
+				token = parsed[p++];
+				arg.default = "";
+
+				var level = 0;
+				while (true)
+				{
+					if (token === "(") ++level;
+					if (token === ")") --level;
+
+					arg.default += token;
+					token = parsed[p++];
+
+					if (level === 0 &&
+					   (token === "," ||
+						token === ")"))
+						break;
+				}
+			}
+
+			line.args.push (arg);
+			if (token !== "," &&
+				token !== ")")
+				throw new Error ("Expecting \",\" or \")\"");
+
+
+			if (token === ",")
+				token = parsed[p++];
+		}
+
+		if (token !== ")")
+			// Expecting end of parameters list
+			throw new Error ("Expecting \")\"");
+		token = parsed[p++];
+
+		// Check const keyword
+		if (token === "const")
+		{
+			line.const = true;
+			token = parsed[p++];
+		}
+
+		if (token !== ";")
+			// Expecting end of line declaration
+			throw new Error ("Expecting \";\"");
+
+		// Must be variable
+		result.push (line);
+		line = { };
+	}
+
+	// Loop through results and assign link names
+	for (var i = result.length - 1; i >= 0; --i)
+	{
+		var name = result[i].name;
+		// Check for separator
+		if (name === undefined)
+			continue;
+
+		if (name.indexOf ("operator") >= 0)
+		{
+			// Properly handle negation
+			if (name === "operator -" &&
+				!result[i].args.length)
+				name = "operator-neg";
+
+			switch (name)
+			{
+				case "operator +=" : name = "operator-add-eq"; break;
+				case "operator -=" : name = "operator-sub-eq"; break;
+				case "operator *=" : name = "operator-mul-eq"; break;
+				case "operator /=" : name = "operator-div-eq"; break;
+
+				case "operator +"  : name = "operator-add";    break;
+				case "operator -"  : name = "operator-sub";    break;
+				case "operator *"  : name = "operator-mul";    break;
+				case "operator /"  : name = "operator-div";    break;
+
+				case "operator &=" : name = "operator-and-eq"; break;
+				case "operator &"  : name = "operator-and";    break;
+				case "operator |=" : name = "operator-or-eq";  break;
+				case "operator |"  : name = "operator-or";     break;
+
+				case "operator <"  : name = "operator-lt";     break;
+				case "operator >"  : name = "operator-gt";     break;
+				case "operator <=" : name = "operator-le";     break;
+				case "operator >=" : name = "operator-ge";     break;
+
+				case "operator ==" : name = "operator-eq";     break;
+				case "operator !=" : name = "operator-ne";     break;
+
+				case "operator ="  : name = "operator-as";     break;
+				case "operator ()" : name = "operator-fn";     break;
+			}
+		}
+
+		// If operator overloading
+		else if (duplex[name] > 1)
+			name += "-" + duplex[name]--;
+
+		// Assign new link name
+		result[i].link = name;
+	}
+
+	return result;
+};
+
+
+
+//----------------------------------------------------------------------------//
 // Grunt                                                                      //
 //----------------------------------------------------------------------------//
 
@@ -114,6 +386,22 @@ module.exports = function (grunt)
 
 					helpers :
 					{
+						// Inline conditional comparison check
+						"ifeq" : function (key, value, content)
+						{
+							return key === value ?
+								content.fn      (this) :
+								content.inverse (this);
+						},
+
+						// Inline conditional comparison check
+						"ifne" : function (key, value, content)
+						{
+							return key !== value ?
+								content.fn      (this) :
+								content.inverse (this);
+						},
+
 						// Inline HTML context editing
 						"context" : function (context)
 						{
@@ -128,18 +416,26 @@ module.exports = function (grunt)
 							this[name] = value.fn (this);
 						},
 
-						// Inline markdown block processing
-						"markdown" : function (name, value)
+						// Inline marked processing
+						"marked" : function (value)
 						{
-							this[name] = mMarked
+							return mMarked
+								(value.fn (this));
+						},
+
+						// Inline source block processing
+						"source" : function (name, value)
+						{
+							this[name] = parseSrc
 								(value.fn (this));
 						}
 					},
 
 					partials :
 					{
-						"partial-menu" : "source/menu.html",
-						"partial-docs" : "source/docs.html"
+						"partial-menu"  : "source/menu.html",
+						"partial-docs"  : "source/docs.html",
+						"partial-table" : "source/table.html"
 					}
 				},
 
